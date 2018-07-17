160bce2f39932cf364867218162a869035dd8062
Rouyer <rouyer_o@etna-alternance.net>
2018-07-17 22:40:25 +0200

    commit linked merkel and django

--- summerhack/accounts/urls.py
+++ summerhack/accounts/urls.py
@@ -10,6 +10,6 @@ urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^home/', views.home, name='home'),
     #url(r'^merkle-tree/', include(admin.site.merkleTree)),
-    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, name='logout'),
 ]
--- summerhack/accounts/views.py
+++ summerhack/accounts/views.py
@@ -2,10 +2,11 @@ from django.contrib.auth import authenticate, login
 from django.views.decorators.csrf import csrf_exempt
 from .form import ConnexionForm
 from django.shortcuts import render, redirect
+from accounts.merkelTree import mt
+import os
 """
     Fonction de connexion / login
     """
-
 
 #@csrf_exempt
 #def login_user(request):
@@ -27,12 +28,19 @@ from django.shortcuts import render, redirect
 #    return render(request, 'registration/login.html', locals())
 
 
+#@csrf_exempt
+#def home(request):
+#    return render(request, 'partials/home.html')
+
+
 @csrf_exempt
 def home(request):
-    return render(request, 'partials/home.html')
-
-
-@csrf_exempt
-def displayCryptoInfos(request):
-    return render(request, 'partials/home.html')
+    buffer = ""
+    mt_a = mt.MarkleTree('testA')
+    # print(mt_a._mt)
+    mt_b = mt.MarkleTree('testB')
+    buffer += "{}{}".format(mt_a.buffer, mt_b.buffer)
+    buffer += mt.MTDiff(mt_a, mt_a._tophash, mt_b, mt_b._tophash)
+    print(buffer)
+    return render(request, 'partials/home.html', locals())
 
