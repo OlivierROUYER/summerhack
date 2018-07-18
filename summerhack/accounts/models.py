5b2d93c7200e0d531a0aedded2aaa997d4db7e31
Rouyer <rouyer_o@etna-alternance.net>
2018-07-18 17:07:05 +0200

    change db in admin and db

--- summerhack/accounts/models.py
+++ summerhack/accounts/models.py
@@ -3,7 +3,8 @@ from django.db import models
 
 class Tree(models.Model):
     db_table = 'tree'
-    date_created = models.DateField(auto_now_add=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     folder_path = models.CharField(max_length=200, blank=False, null=False)
     key = models.CharField(max_length=100, blank=False, null=False)
     #compare = models.CharField(max_length=10, blank=False, null=False)
