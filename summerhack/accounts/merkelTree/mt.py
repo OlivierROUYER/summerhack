228ca93f62ef7b5d5618121caedbe8f3c00fa11b
Rouyer <rouyer_o@etna-alternance.net>
2018-07-19 22:48:56 +0200

    tang

--- summerhack/accounts/merkelTree/mt.py
+++ summerhack/accounts/merkelTree/mt.py
@@ -162,23 +162,19 @@ def MTDiff(mt_a, a_tophash, mt_b, b_tophash):
 def TestIfExist(mt_a):
    tophash = mt_a._tophash
    buffer = []
-   if Tree.objects.all().filter(folder_path=os.getcwd() + "/" + mt_a._root).latest('created_at'):
-    tree_b = Tree.objects.all().filter(folder_path=os.getcwd() + "/" + mt_a._root).latest('created_at')
-   else:
-       return ['No Folder found for testing']
+   tree_b = Tree.objects.all().filter(folder_path=os.getcwd() + "/" + mt_a._root).latest('created_at')
    if tree_b is None or tree_b.key != tophash:
     a_value = mt_a._mt[tophash]
     a_child = a_value[1]
     for itemhash, item in a_child.iteritems():
         mt_string = os.getcwd() + "/" + mt_a._root + "/" + item
-        if Tree.objects.all().filter(folder_path=mt_string).latest('created_at'):
-            tree_c = Tree.objects.all().filter(folder_path=mt_string).latest('created_at')
-            if tree_c.key != itemhash:
-                buffer.append("File {} modified :\nOld -> {}\n Actual -> {}\n".format(item, tree_c.key, itemhash))
-            else:
-                buffer.append("File {} not modified\n".format(item))
+        print(mt_string)
+        tree_c = Tree.objects.all().filter(folder_path=mt_string).latest('created_at')
+        print(mt_string)
+        if tree_c.key != itemhash:
+            buffer.append("File {} modified :\nOld -> {}\n Actual -> {}\n".format(item, tree_c.key, itemhash))
         else:
-            return []
+            buffer.append("File {} not modified\n".format(item))
    else:
-       buffer = []
+       buffer.append("No modification\n")
    return buffer
