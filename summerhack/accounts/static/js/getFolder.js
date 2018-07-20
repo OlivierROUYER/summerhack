610a40541fe782ba1f702ebae7bdec93952cd220
Tang8 <tang_g@etna-alternance.net>
2018-07-20 07:03:06 -0400

    fwefwef

--- summerhack/accounts/static/js/getFolder.js
+++ summerhack/accounts/static/js/getFolder.js
@@ -1,5 +1,8 @@
 $(document).ready(function() {
 
+var resultscan;
+var AllFile;
+    
 setInterval(function () {
 	var settings = {
 	    "async": true,
@@ -12,24 +15,27 @@ setInterval(function () {
 		"Postman-Token": "ca0e836a-4d6e-479e-bf1d-a50b2a5c8e3a"
 	    }
 	};
-  $("tr[id*='Destroyit']").each(function (i, el) {
-      el.remove();
-  });
   $.ajax(settings).done(function getResult(response) {
-	   var data = response.tree_list;
-     data.forEach(function (element, index) {
-       var node = document.createElement("TR");
-       node.setAttribute("id", "Destroyit");
-       var mess = "test";
-       var content = document.createTextNode(mess);
-       node.appendChild(content);
+      var data = response.tree_list;
+      if (data != AllFile) {
+      	AllFile = data;
+	$("tr[id*='Destroyit']").each(function (i, el) {
+	    el.remove();
+	});
+     	data.forEach(function (element, index) {
+       	var node = document.createElement("TR");
+       	node.setAttribute("id", "Destroyit");
+       	var mess = "test";
+       	var content = document.createTextNode(mess);
+       	node.appendChild(content);
 
-       node.innerHTML = "<td>" + element.folder_path + "</td>" +
-       "<td>" + element.key + "</td>" +
-       "<td>" + element.created_at + "</td>" +
-       "<td>" + element.updated_at + "</td>";
-       $(".key-file").append(node);
-     });
+       	node.innerHTML = "<td>" + element.folder_path + "</td>" +
+       	"<td>" + element.key + "</td>" +
+       	"<td>" + element.created_at + "</td>" +
+       	"<td>" + element.updated_at + "</td>";
+	 $(".key-file").append(node);
+     	});
+      }
   })}, 5000);
 
 setInterval(function () {
@@ -43,10 +49,7 @@ setInterval(function () {
   		  "Cache-Control": "no-cache",
   		  "Postman-Token": "ca0e836a-4d6e-479e-bf1d-a50b2a5c8e3a"
   	   }
-  	};
-    $("div[class*='message']").each(function (i, el) {
-	el.remove();
-    });
+	};
     $.ajax(settings).done(function getResult(response) {
   	   var data = response.buffer2;
        if (data.length != 0) {
@@ -55,10 +58,18 @@ setInterval(function () {
          var mess = " ";
          var content = document.createTextNode(mess);
          var datatmp = JSON.stringify(data);
-         datatmp = datatmp.replace(/["[\]]/gm, "<br>");
-         node.innerHTML = datatmp;
-         node.appendChild(content);
-         $(".resultScan").append(node);
+         datatmp = datatmp.replace(/["[\]]/gm, "");
+	 datatmp = datatmp.replace(/\\n/gm, "<br>");
+	 datatmp = datatmp.replace(/,/gm, "");
+	 if (resultscan != datatmp) {
+	     $("div[class*='message']").each(function (i, el) {
+		el.remove();
+	     });
+             node.innerHTML = datatmp;
+             node.appendChild(content);
+             $(".resultScan").append(node);
+	     resultscan = datatmp;
+	  }
        } else {
          var node = document.createElement("DIV");
          node.setAttribute('class', 'message alert alert-success');
