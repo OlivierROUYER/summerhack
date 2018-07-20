b69d43f6824973595d4023820cd3216d082f360f
Tang8 <tang_g@etna-alternance.net>
2018-07-20 05:17:21 -0400

    fewf

--- summerhack/accounts/static/js/getFolder.js
+++ summerhack/accounts/static/js/getFolder.js
@@ -1,37 +1,20 @@
 $(document).ready(function() {
-  var settings = {
-   "async": true,
-   "crossDomain": true,
-   "url": "http://172.16.15.7:8000/tree/",
-   "method": "GET",
-   "headers": {
-     "Authorization": "Basic cm9vdDpyb290",
-     "Cache-Control": "no-cache",
-     "Postman-Token": "ca0e836a-4d6e-479e-bf1d-a50b2a5c8e3a"
-   }
-  };
-
-  // $.ajax(settings).done(function getResult(response) {
-  // var data = response.tree_list;
-  //   data.forEach(function (element, index) {
-  //     var node = document.createElement("TR");
-  //     var mess = "test";
-  //     var content = document.createTextNode(mess);
-  //     node.appendChild(content);
-  //
-  //     node.innerHTML = "<td>" + element.folder_path + "</td>" +
-  //     "<td>" + element.key + "</td>" +
-  //     "<td>" + element.created_at + "</td>" +
-  //     "<td>" + element.updated_at + "</td>";
-  //     $(".key-file").append(node);
-  //     console.log(element);
-  //     console.log(index);
-  //   });
-  // });
-
-  setInterval(function (settings) {
+     setInterval(function () {
+	var settings = {
+	    "async": true,
+	    "crossDomain": true,
+	    "url": "http://172.16.15.7:8000/tree/",
+	    "method": "GET",
+	    "headers": {
+		"Authorization": "Basic cm9vdDpyb290",
+		"Cache-Control": "no-cache",
+		"Postman-Token": "ca0e836a-4d6e-479e-bf1d-a50b2a5c8e3a"
+	    }
+	};
+	 
      $.ajax(settings).done(function getResult(response) {
-     var data = response.tree_list;
+	 var data = response.tree_list;
+	 console.log(data);
      data.forEach(function (element, index) {
        var node = document.createElement("TR");
        var mess = "test";
